8dec4ed8edfa80f747bba342220286066c1c511c
YakirYehuda <yy2006@gmail.com>
2019-05-14 21:15:04 +0300

    Added crane id as a view parameter

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,20 +10,19 @@ import android.widget.EditText;
 import com.example.myapplication.server.CraneViewController;
 import com.example.myapplication.server.ServerHelper;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class MainActivity extends AppCompatActivity {
 
     private EditText mServerAddress;
     private ServerHelper mServerHelper;
     private CraneViewController mCraneViewController;
+    private EditText mCraneId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mServerAddress = findViewById(R.id.server_id);
+        mCraneId = findViewById(R.id.crane_id);
         mServerHelper = new ServerHelper(this);
         mCraneViewController = new CraneViewController(this, (TopCraneInfoView) findViewById(R.id.top_crane_view), new Handler(Looper.getMainLooper()), mServerHelper);
     }
@@ -34,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void setServerAddress(View view) {
         String address = mServerAddress.getText().toString();
-        mServerHelper.setAddress(address);
+        String craneId = mCraneId.getText().toString();
+        mServerHelper.set(address, craneId);
     }
 }
